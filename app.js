0a40eaad10f458f1abaf6a5b85afec4956936c1b
harsh <harsh.swami0@gmail.com>
2023-10-20 18:03:29 +0530

    successfully added logs for info and debug

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const app = Express();
 const PORT=4000;
 
 app.get('/',(req,res)=>{
+    logger.info("this is / route called");
     res.status(200).send('welcome to my app');
 })
 
 
 app.get('/gotError',(req,res)=>{
 try {
-    
+    logger.info("got error");
     throw new Error("got Error");
 } catch (error) {
-    console.log("sommething went wrong! Try Again");
+    logger.debug("sommething went wrong! Try Again");
     res.status(500).send('Caught Error');
 }
 })
 
 
-app.listen(PORT,()=>{console.log("server started on port 4000")});
+app.listen(PORT,()=>{logger.info("server started on port 4000")});
--- logger.js
+++ logger.js
@@ -22,6 +22,7 @@ const options={
     }
 }
 
+
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
@@ -40,11 +41,12 @@ const logger = winston.createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   logger.add(new winston.transports.Console({
+//     format: winston.format.simple(),
+//   }));
+
+
 
 
 export default logger;
